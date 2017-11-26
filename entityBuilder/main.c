ab6ebbd1a00478d61e0c1f8c89271f5d832d7930
Mauricio Davila <Mauro@MacBook-Pro-de-Mauricio.local>
2017-11-26 20:05:18 -0300

    add -> IFDEF

--- entityBuilder/main.c
+++ entityBuilder/main.c
@@ -16,9 +16,20 @@ int buildSettersPrototypes(char* entityName, Member* members,int qtyMembers, cha
 int buildGetters(char* entityName, Member* members,int qtyMembers, char* result);
 int buildGettersPrototypes(char* entityName, Member* members,int qtyMembers, char* result);
 
+void strToUpper(char *str,char *strUp)
+{
+  while(*str != '\0')
+  {
+    *strUp = toupper(*str);
+    str++;
+    strUp++;
+  }
+}
+
 int main(int argc, char **argv)
 {
     char entityName[256];
+    char entityNameUp[256];
     Member members[256];
     int qtyMembers;
     int i,j;
@@ -40,16 +51,19 @@ int main(int argc, char **argv)
         }
     }
 
-
+    strToUpper(entityName,entityNameUp);
     printf("\n\n********************* %s.h ************************\n\n",entityName);
+    printf("#ifndef _%s_H\n#define _%s_H\n",entityNameUp,entityNameUp);  // _EMPLOYEE_H
     buildStruct(entityName, members, qtyMembers, result);
     printf("%s",result);
+    printf("#endif// _%s_H\n\n",entityNameUp);
     buildConstructorPrototype(entityName, members, qtyMembers, result);
     printf("%s",result);
     buildSettersPrototypes(entityName, members, qtyMembers, result);
     printf("%s",result);
     buildGettersPrototypes(entityName, members, qtyMembers, result);
     printf("%s",result);
+    printf("\n\n********************* %s.h ************************\n\n",entityName);
 
     printf("\n\n********************* %s.c ************************\n\n",entityName);
     buildConstructor(entityName, members, qtyMembers, result);
@@ -59,6 +73,7 @@ int main(int argc, char **argv)
     buildGetters(entityName, members, qtyMembers, result);
     printf("%s",result);
 
+    printf("\n\n********************* %s.c ************************\n\n",entityName);
 
 
     exit(EXIT_SUCCESS);
@@ -174,7 +189,7 @@ int buildConstructor(char* entityName, Member* members,int qtyMembers, char* res
         strcat(result,auxString);
     }
 
-    sprintf(auxString,"\treturn this;\n}\n", entityName);
+    sprintf(auxString,"\treturn this;\n}\n\n", entityName);
     strcat(result,auxString);
     return 0;
 }
@@ -203,9 +218,9 @@ int buildSetters(char* entityName, Member* members,int qtyMembers, char* result)
 
 
         if(flagArray)
-            sprintf(auxString,"int %sset%s(%s* this,%s* %s)\n{\n\tstrcpy(this->%s,%s);\n\treturn 0;\n}\n", auxPrefix,auxDefinitionUp,entityName,members[i].type,auxDefinition,auxDefinition,auxDefinition);
+            sprintf(auxString,"int %sset%s(%s* this,%s* %s)\n{\n\tstrcpy(this->%s,%s);\n\treturn 0;\n}\n\n", auxPrefix,auxDefinitionUp,entityName,members[i].type,auxDefinition,auxDefinition,auxDefinition);
         else
-            sprintf(auxString,"int %sset%s(%s* this,%s %s)\n{\n\tthis->%s = %s;\n\treturn 0;\n}\n", auxPrefix,auxDefinitionUp,entityName,members[i].type,auxDefinition,auxDefinition,auxDefinition);
+            sprintf(auxString,"int %sset%s(%s* this,%s %s)\n{\n\tthis->%s = %s;\n\treturn 0;\n}\n\n", auxPrefix,auxDefinitionUp,entityName,members[i].type,auxDefinition,auxDefinition,auxDefinition);
 
         strcat(result,auxString);
     }
@@ -270,9 +285,9 @@ int buildGetters(char* entityName, Member* members,int qtyMembers, char* result)
             sprintf(auxDefinitionUp,"%c",toupper(auxDefinition[0]));
 
         if(flagArray)
-            sprintf(auxString,"%s* %sget%s(%s* this)\n{\n\treturn this->%s;\n}\n", members[i].type, auxPrefix,auxDefinitionUp,entityName,auxDefinition,auxDefinition);
+            sprintf(auxString,"%s* %sget%s(%s* this)\n{\n\treturn this->%s;\n}\n\n", members[i].type, auxPrefix,auxDefinitionUp,entityName,auxDefinition,auxDefinition);
         else
-           sprintf(auxString,"%s %sget%s(%s* this)\n{\n\treturn this->%s;\n}\n", members[i].type, auxPrefix,auxDefinitionUp,entityName,auxDefinition,auxDefinition);
+           sprintf(auxString,"%s %sget%s(%s* this)\n{\n\treturn this->%s;\n}\n\n", members[i].type, auxPrefix,auxDefinitionUp,entityName,auxDefinition,auxDefinition);
 
 
         strcat(result,auxString);
